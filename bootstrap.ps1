98b4d6ef4883d587e4c45d2c4b40ec0bc686eab9
Adam <55899222+UniverseCraft@users.noreply.github.com>
2023-04-12 20:51:01 -0500

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -6,9 +6,9 @@ $unattend = Get-Content "C:\Windows\Panther\unattend.xml"
 $m = $xml | Select-String -Pattern "githubusercontent\.com\/([a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+)\/main"
 $gh_repo_id = $m.Matches.Groups[1].Value
 
-"Downloading from GitHub repo: $gh_repo_id"
-curl -o PowerWash.ps1 https://raw.githubusercontent.com/$gh_repo_id/main/PowerWash.ps1
-curl -o PowerWashSettings.json https://raw.githubusercontent.com/$gh_repo_id/main/PowerWashSettings.json
+"Downloading from GitHub repo: $gh_repo_id$"
+curl -o PowerWash.ps1 https://raw.githubusercontent.com/$gh_repo_id$/main/PowerWash.ps1
+curl -o PowerWashSettings.json https://raw.githubusercontent.com/$gh_repo_id$/main/PowerWashSettings.json
 Set-ExecutionPolicy Unrestricted -Force -Confirm:$false
 
 "Bootstrapping into PowerWash..."
