5f820763a4175326cb50e3c11c9b11f5c793e98d
Adam <55899222+UniverseCraft@users.noreply.github.com>
2023-05-14 22:53:39 -0700

    Create bootstrap-manual.ps1

---
+++ autodeploy/bootstrap-manual.ps1
@@ -0,0 +1,14 @@
+# "PowerShell.exe" -Command "curl -o bootstrap.ps1 https://raw.githubusercontent.com/UniverseCraft/WindowsPowerWash/main/autodeploy/bootstrap-manual.ps1; Set-ExecutionPolicy Unrestricted -Force -Confirm:$false; .\bootstrap-manual.ps1 -Confirm:$false"
+
+
+"PowerWash initializing..."
+
+$gh_repo_id = "UniverseCraft/WindowsPowerWash"
+
+"Downloading from GitHub repo: $gh_repo_id"
+curl -o PowerWash.ps1 "https://raw.githubusercontent.com/$gh_repo_id/main/PowerWash.ps1"
+curl -o PowerWashSettings.yml "https://raw.githubusercontent.com/$gh_repo_id/main/PowerWashSettings.yml"
+Set-ExecutionPolicy Unrestricted -Force -Confirm:$false
+
+"Bootstrapping into PowerWash..."
+.\PowerWash.ps1 -Confirm:$false
