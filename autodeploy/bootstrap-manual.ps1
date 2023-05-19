7abf1e52835ee19c409da77e53c99152b1cce7d5
Adam <55899222+UniverseCraft@users.noreply.github.com>
2023-05-19 02:34:37 -0500

    Update bootstrap-manual.ps1

--- autodeploy/bootstrap-manual.ps1
+++ autodeploy/bootstrap-manual.ps1
@@ -1,6 +1,5 @@
 # "PowerShell.exe" -Command "curl -o bootstrap-manual.ps1 https://raw.githubusercontent.com/UniverseCraft/WindowsPowerWash/main/autodeploy/bootstrap-manual.ps1; Set-ExecutionPolicy Unrestricted -Force -Confirm:$false; .\bootstrap-manual.ps1 -Confirm:$false"
 
-
 "PowerWash initializing..."
 
 $gh_repo_id = "UniverseCraft/WindowsPowerWash"
@@ -10,5 +9,13 @@ curl -o PowerWash.ps1 "https://raw.githubusercontent.com/$gh_repo_id/main/PowerW
 curl -o PowerWashSettings.yml "https://raw.githubusercontent.com/$gh_repo_id/main/PowerWashSettings.yml"
 Set-ExecutionPolicy Unrestricted -Force -Confirm:$false
 
+Read-Host "To modify the PowerWash configuration, edit PowerWashSettings.yml in the current directory, then press Enter"
+$mode = Read-Host "Enter 'manual' to be promoted whether you want to apply each PowerWash feature, or 'config' to follow the configured settings."
+
 "Bootstrapping into PowerWash..."
-.\PowerWash.ps1 -Confirm:$false
+if ($mode -eq "config") {
+    .\PowerWash.ps1 /config -Confirm:$false
+}
+else {
+    .\PowerWash.ps1 -Confirm:$false
+}
