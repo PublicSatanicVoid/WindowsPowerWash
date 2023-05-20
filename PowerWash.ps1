f679b0386fc0b9bff5630fa59606ea31a1e75d81
Adam <55899222+UniverseCraft@users.noreply.github.com>
2023-05-19 20:53:21 -0500

    Update PowerWash.ps1

--- PowerWash.ps1
+++ PowerWash.ps1
@@ -423,7 +423,7 @@ function UnpinApp($appname) {
 	((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() `
     | Where-Object { $_.Name -eq $appname }).Verbs() `
     | Where-Object { $_.Name.replace('&', '') -match 'Unpin from taskbar' } `
-    | ForEach-Object { $_.DoIt() } | 2>$null | Out-Null
+    | ForEach-Object { $_.DoIt() } 2>$null | Out-Null
 }
 
 function CreateShortcut($Dest, $Source, $Admin = $false) {
